ef0ea0be97d2e20957eb9297f0e52d87c09f7605
Mohsen Karimi <mohsenk.work@gmail.com>
2026-05-03 18:41:24 +0200

    add API Explorer catalog page with OpenAPI specifications and dynamic card display

---
+++ src/utils/html.ts
@@ -0,0 +1,11 @@
+export function escapeHtml(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+}
+
+export function escapeAttr(str: string): string {
+  return str.replace(/"/g, "&quot;").replace(/'/g, "&#39;");
+}
